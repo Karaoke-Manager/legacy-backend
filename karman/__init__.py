ceff6788e45cdb823f91762dd09088fd34c6a68c
Kim Wittenburg <codello@wittenburg.kim>
2020-03-22 00:48:46 +0100

    Update CI

--- karman/__init__.py
+++ karman/__init__.py
@@ -1,6 +1,6 @@
 from flask_jwt_extended import JWTManager
 
-from .models import db
-from .rest import api
+from .models import db  # noqa: F401
+from .rest import api  # noqa: F401
 
 jwt = JWTManager()
